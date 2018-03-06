12da218e64f231516bdc572df76d929c6e1b9615
cale312 <zikisacalesogaxa@gmail.com>
2018-03-06 16:26:21 +0200

    changed some syntax;

--- src/test/greet.test.ts
+++ src/test/greet.test.ts
@@ -1,7 +1,7 @@
 import greeter from '../libs/greet';
-import * as assert from 'assert'
+import * as assert from 'assert';
 
-describe('simple greet function test', () => {
+describe('Greet function test', () => {
 
     it('returns a greeting to the passed in name in the function', (done: any) => {
         assert.equal("Hello World", greeter("World"));
