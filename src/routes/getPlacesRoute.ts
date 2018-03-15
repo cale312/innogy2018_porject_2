c031a62f0add61dcf74ff5eef140170a5f58daf3
cale312 <zikisacalesogaxa@gmail.com>
2018-03-15 10:15:08 +0200

    added extension so it returns also the related table data from the reviews table;

--- src/routes/getPlacesRoute.ts
+++ src/routes/getPlacesRoute.ts
@@ -23,7 +23,7 @@ class Route {
         // create repository for the places entity
         const _placesRepository = await getRepository(Place);
         await _placesRepository
-            .find()
+            .find({ relations: ["Reviews"]})
             .then(async (places: any) => {
                 res.json({
                     code,
