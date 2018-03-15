83ca08b2e76cf6bcc5e41938d0f17a93741f12f1
cale312 <zikisacalesogaxa@gmail.com>
2018-03-15 10:14:20 +0200

    added joinTable builtin function from typeorm;

--- src/entity/Place.entity.ts
+++ src/entity/Place.entity.ts
@@ -2,7 +2,8 @@ import {
     Entity,
     PrimaryGeneratedColumn,
     Column,
-    OneToMany
+    OneToMany,
+    JoinColumn
 } from 'typeorm';
 import {
     Reviews
@@ -40,6 +41,7 @@ export class Place {
     Dislikes: number;
 
     @OneToMany(type => Reviews, reviews => reviews.Place)
-    Reviews: Reviews[]
+    @JoinColumn()
+    Reviews: Reviews[];
 
 }
