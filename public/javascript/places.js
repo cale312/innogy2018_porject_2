421346e122a793d5f7e4ca3598a761d90df8e51e
cale312 <zikisacalesogaxa@gmail.com>
2018-03-26 12:35:26 +0200

    changed the password, and added functionality for using the checkboxes for filtering the places view, changed some variable names and added comments on the code;

--- public/javascript/places.js
+++ public/javascript/places.js
@@ -1,7 +1,6 @@
   var apiURL = `http://${window.location.hostname}:8000/api/v1/places/`;
-  let AllRecords = document.querySelector(".listOfPlaces");
   let categories = [];
-  let catMap = {};
+  let categoryMap = {};
   let allData = null;
   let placesObj = {};
   let placeToReview = [];
@@ -12,7 +11,7 @@
     });
   });
 
-  var placesMap = function initMap(latitude, longitude, zoom) { //This function creates another initial map
+  var placesMap = function initMap(latitude, longitude, zoom) { // This function creates another initial map
 
     var uluru = {
       lat: -25.363,
@@ -26,15 +25,13 @@
       position: place,
       map: map
     });
-  }
+  };
 
 
-  function AppViewmodel() {  //creates a knockoutJS viewmodel
+  function AppViewmodel() { // creates a knockoutJS viewmodel
     var self = this;
     self.categories = ko.observable();
-    self.place = ko.observable();
     self.places = ko.observable();
-    self.matches = ko.observable();
     self.loading = ko.observable(`<div class="progress black" style="visibility: hidden;margin-top: 0;"><div class="indeterminate white"></div></div>`);
     self.data = ko.observable(false);
     self.review_data = ko.observable(false);
@@ -42,11 +39,13 @@
     self.visits = ko.observable();
     self.selectedPlaceReviews = ko.observable();
 
-    var placeReviwing = null; //
+    let placeReviwing; // cache the data of the place to review
+    let matchedPlaces = []; // stores the searched matched places
+    let matchedPlacesMap = {}; // maps the filtering categories
 
     self.loading(`<div class="progress black" style="margin-top: 0;"><div class="indeterminate white"></div></div>`);
 
-    //get all the places that are stored in the database
+    // get all the places that are stored in the database
     $.getJSON(apiURL, (data) => {
       setTimeout(() => {
         // caching the data for easy access
@@ -60,12 +59,12 @@
         self.places(allData);
         allData.map((place) => {
           (placesObj[place.name] === undefined) ? placesObj[place.name] = null: false;
-          if (catMap[place.category] === undefined) {
-            catMap[place.category] = null;
+          if (categoryMap[place.category] === undefined) {
+            categoryMap[place.category] = null;
             categories.push(place.category.split("_").join(" "));
           }
         });
-        // autocomplete
+        //  autocomplete
         $('input.autocomplete').autocomplete({
           data: placesObj,
           limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
@@ -81,80 +80,76 @@
     });
 
     self.search = () => {
-      var matches = [];
+      matchedPlaces = [];
+
+      // if there is no text in the search box display all the places
+      $('#pac-input').on('keyup', () => {
+        ($('#pac-input').val().trim() == 0) ? self.places(allData): false;
+      });
+
       let placeName = $('#pac-input').val();
       if (placeName.trim().length != 0) {
         allData.map((place) => {
           if (placeName.toLowerCase() === place.name.toLowerCase()) {
-            matches.push(place);
+            matchedPlaces.push(place);
           }
-        })
-        self.places(matches);
+        });
+        self.places(matchedPlaces);
         return;
       } else {
         Materialize.toast("please enter valid place", 1500);
       }
-      console.log('found...', matches);
+      console.log('found...', matchedPlaces);
       self.places(allData);
-    }
-
-    let placeForReviews = null;
+    };
 
     self.reviews = (evt) => {
       console.log('clicked on', evt);
       let thePlace = [];
-      placeForReviews = evt;
+      placeReviwing = evt;
 
       self.loading(`<div class="progress black" style="margin-top: 0;"><div class="indeterminate white"></div></div>`);
       self.data(false);
 
       allData.map((place) => {
-        if (place.name === evt.name) {
-          placeReviwing = evt.name;
+        if (place.name === placeReviwing.name) {
+          placeReviwing = placeReviwing.name;
           self.selectedPlace(place);
-          self.selectedPlaceReviews(place.reviews)
+          self.selectedPlaceReviews(place.reviews);
         }
-      })
+      });
 
       setTimeout(() => {
         self.review_data(true);
         self.loading(`<div class="progress black" style="visibility: hidden;margin-top: 0;"><div class="indeterminate white"></div></div>`);
-        var lat = parseFloat(evt.lat);
-        var lng = parseFloat(evt.lng);
+        var lat = parseFloat(placeReviwing.lat);
+        var lng = parseFloat(placeReviwing.lng);
 
-        console.log('clicked on', evt.lat);
-        console.log('clicked on', evt.lng);
+        console.log('clicked on', placeReviwing.lat);
+        console.log('clicked on', placeReviwing.lng);
         initMap(lat, lng, 18);
 
-        self.visits(`${evt.visits} people visited this place`);
+        self.visits(`${placeReviwing.visits} people visited this place`);
 
-        document.getElementById("send-review").disabled = true;
-
-        $('#Username, #reviewInfo').on('keyup', () => {
-          ($('#Username').val().length > 0 && $("#reviewInfo").val().length > 0) ? document.getElementById("send-review").disabled = false: document.getElementById("send-review").disabled = true;
-        })
       }, 1500);
 
-    }
+    };
 
-    self.back = () => {
-      self.loading(`<div class="progress black" style="margin-top: 0;"><div class="indeterminate white"></div></div>`);
-      self.review_data(false);
-      setTimeout(() => {
-        self.data(true);
-        self.loading(`<div class="progress black" style="visibility: hidden;margin-top: 0;"><div class="indeterminate white"></div></div>`);
-      }, 1500);
-    }
+    self.submitReview = () => {
 
-    self.sendReview = () => {
-      console.log("IM HAPPENING");
-      
+      document.getElementById("send-review").disabled = true; // disable comment button
+
+      // enable submit button if both input fields filled
+      $('#Username, #reviewInfo').on('keyup', () => {
+        ($('#Username').val().length > 0 && $("#reviewInfo").val().length > 0) ? document.getElementById("send-review").disabled = false: document.getElementById("send-review").disabled = true;
+      });
+
+      // cache the usernaame and review text from the dom
       var reviewData = {
-        userName:$('#Username').val(),
+        userName: $('#Username').val(),
         review: $("#reviewInfo").val()
-      }
-      console.log(placeReviwing);
-      
+      };
+
       $.ajax(apiURL + placeReviwing + '/review', {
         data: JSON.stringify(reviewData),
         type: "POST",
@@ -164,20 +159,26 @@
             setTimeout(() => {
               // caching the data for easy access
               allData = data.places;
-      
+
+              // if there is no data in the database, redirect to the adding places page
               if (allData.length === 0) {
                 window.location = "search.html";
                 return;
               }
-      
+
               self.places(allData);
+
+              // creating the check box categories for easy fiiltering
               allData.map((place) => {
+                // maps the already pushed categories to stop possible diplications
                 (placesObj[place.name] === undefined) ? placesObj[place.name] = null: false;
-                if (catMap[place.category] === undefined) {
-                  catMap[place.category] = null;
+                if (categoryMap[place.category] === undefined) {
+                  categoryMap[place.category] = null;
+                  // remove the underscores from the text before pushing
                   categories.push(place.category.split("_").join(" "));
                 }
               });
+
               // autocomplete
               $('input.autocomplete').autocomplete({
                 data: placesObj,
@@ -187,21 +188,63 @@
                 },
                 minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
               });
+
               self.data(true);
               self.loading('');
               self.categories(categories);
+
               allData.map((place) => {
                 if (place.name === placeForReviews.name) {
                   placeReviwing = placeForReviews.name;
                   self.selectedPlace(place);
                   self.selectedPlaceReviews(place.reviews)
                 }
-              })
+              });
             }, 1500);
           });
         },
       });
-    }
+    };
+
+    // logic for filtering by checkboxes
+    $(document).on('click', '[type=checkbox]', function (e) {
+      let category = e.target.id.toLowerCase().trim().replace(" ", "_"); // cache the category of the checkbox
+
+      if (document.getElementById(e.target.id).hasAttribute("checked") === false) { // if the checkbox is not checked, check it using javascripts setAttribute
+        document.getElementById(e.target.id).setAttribute("checked", "checked");
+
+        // push the checked places iterated from all the data from the database to the matches array
+        allData.map((place) => {
+          (place.category === category) ? matchedPlaces.push(place): false;
+        });
+
+      } else {
+        // unchecking the box, removes it from the array of the places
+        document.getElementById(e.target.id).removeAttribute("checked");
+
+        // maps the matchedPlaces array and looks for the place with the same category to splice
+        matchedPlaces.map( (place) => {
+          let removeCategories = place.category !== category;
+          matchedPlaces.splice(removeCategories, 1);
+          console.log('unsee category', place.name);
+        });
+
+      };
+
+      (matchedPlaces.length > 0) ? self.places(matchedPlaces): self.places(allData);
+
+    });
+
+
+    self.back = () => {
+      self.loading(`<div class="progress black" style="margin-top: 0;"><div class="indeterminate white"></div></div>`);
+      self.review_data(false);
+      self.data(false);
+      setTimeout(() => {
+        self.data(true);
+        self.loading(`<div class="progress black" style="visibility: hidden;margin-top: 0;"><div class="indeterminate white"></div></div>`);
+      }, 1500);
+    };
 
   };
 
@@ -209,16 +252,14 @@
     let data = {
       lat: -33.918861,
       lng: 18.423300
-    }
+    };
 
     if (latitude && longitude) {
       data = {
         lat: latitude,
         lng: longitude
-      }
+      };
     }
-
-    console.log("DATA", data);
 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: zoom,
@@ -228,6 +269,6 @@
       position: data,
       map: map
     });
-  }
+  };
 
   ko.applyBindings(new AppViewmodel());
